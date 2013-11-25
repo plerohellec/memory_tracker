03ac0a195e5736f6a57d9c302bb58e2af6b0abfe
Philippe Le Rohellec <philippe@lerohellec.com>
2013-11-24 17:35:39 -0800

    fallback to gem config file in case the app config file is missing

--- lib/memory_tracker/engine.rb
+++ lib/memory_tracker/engine.rb
@@ -5,13 +5,22 @@ module MemoryTracker
     engine_base_dir = File.expand_path("../../..",     __FILE__)
     app_base_dir    = File.expand_path("../../../app", __FILE__)
     lib_base_dir    = File.expand_path("../../../lib", __FILE__)
+    config_base_dir = File.expand_path("../../../config", __FILE__)
 
     config.autoload_paths << lib_base_dir
 
 
     initializer "memory_tracker.load_config" do |app|
-      config_file_path = "#{app.root}/config/memory_tracker.yml"
-      if File.exist?(config_file_path)
+      app_config_file_path = "#{app.root}/config/memory_tracker.yml"
+      gem_config_file_path = "#{config_base_dir}/memory_tracker.yml"
+      if File.exist?(app_config_file_path)
+        config_file_path = app_config_file_path
+      elsif File.exist?(gem_config_file_path)
+        config_file_path = gem_config_file_path
+      end
+
+      if config_file_path
+        Rails.logger.debug "Reading #{config_file_path}"
         config = YAML.load_file(config_file_path)
         config = config.fetch(Rails.env) { raise "#{Rails.env} environment key is missing from #{config_file_path}" }
         MemoryTracker.instance.config = ActiveSupport::HashWithIndifferentAccess.new(config)
